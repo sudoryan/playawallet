df2385bac40208f70f23426cc185c9331c06c4ef
ryan <rleinbox@gmail.com>
2017-08-11 21:16:13 -0700

    clean repo

--- config/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-	session_start();
-	$_SESSION["loggued_on_user"] = False;
-	header("Location: /");
-?>
--- recovery/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-	<link href='/css/style.css' rel='stylesheet' type='text/css'>
-	<title>Recover your account</title>
-</head>
-<body>
-	<a href="/">Home</a>
-</body>
-</html>
